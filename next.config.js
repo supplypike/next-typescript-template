3002f213a30c26b650713961eef1227c2539e297
Charles <charleshh1995@gmail.com>
2019-02-26 17:14:45 -0600

    Init commit

--- next.config.js
+++ next.config.js
@@ -1,19 +1,26 @@
-const withTypescript         = require('@zeit/next-typescript')
-const withCSS                = require('@zeit/next-css')
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
-
+const withTypescript = require('@zeit/next-typescript');
+const withCSS = require('@zeit/next-css');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const withImages = require('next-images');
+const withSass = require('@zeit/next-sass');
 module.exports = withTypescript(
-  withCSS({
-    webpack(config) {
-      if (process.env.ANALYZE) {
-        config.plugins.push(new BundleAnalyzerPlugin({
-          analyzerMode: 'server',
-          analyzerPort: 8888,
-          openAnalyzer: true
-        }))
-      }
-      return config
-    },
-    cssModules: true,
-  })
-)
+  withImages(
+    withSass(
+      withCSS({
+        webpack(config) {
+          if (process.env.ANALYZE) {
+            config.plugins.push(
+              new BundleAnalyzerPlugin({
+                analyzerMode: 'server',
+                analyzerPort: 8888,
+                openAnalyzer: true
+              })
+            );
+          }
+          return config;
+        },
+        cssModules: true
+      })
+    )
+  )
+);
