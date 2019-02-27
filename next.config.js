a1223c9fa7dff42df8b547cb47b8835cf2010669
Charles <charles@supplypike.com>
2019-02-27 16:15:33 -0600

    Fix CSS

--- next.config.js
+++ next.config.js
@@ -1,25 +1,27 @@
-const withTypescript = require('@zeit/next-typescript');
-const withCSS = require('@zeit/next-css');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const withImages = require('next-images');
-const withSass = require('@zeit/next-sass');
-module.exports = withTypescript(
-  withImages(
-    withSass(
-      withCSS({
+const withTypescript = require("@zeit/next-typescript");
+const withCSS = require("@zeit/next-css");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const withImages = require("next-images");
+const withSass = require("@zeit/next-sass");
+
+module.exports = withCSS(
+  withTypescript(
+    withImages(
+      withSass({
         webpack(config) {
+          console.log(process.env.ANALYZE);
           if (process.env.ANALYZE) {
             config.plugins.push(
               new BundleAnalyzerPlugin({
-                analyzerMode: 'server',
+                analyzerMode: "server",
                 analyzerPort: 8888,
                 openAnalyzer: true
               })
             );
           }
           return config;
-        },
-        cssModules: true
+        }
+        // cssModules: true
       })
     )
   )
